2ce80cc2a6cc3ebef0ee2647059365a77d80baaa
bigbeno37 <contact@benosullivan.com>
2022-05-03 17:53:55 +1000

    Added eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = document.getElementById('root');
+
+if (!root) throw new Error('Element #root not found! Something has gone terribly, terribly wrong!');
+
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
