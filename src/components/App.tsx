3009a0283f38238553e6314be8ce0adbb1e4fb36
bigbeno37 <contact@benosullivan.com>
2022-05-04 18:18:04 +1000

    Refactored a number of GoL pure functions into separate utils.ts file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,8 @@
 import {useEffect, useMemo, useState} from 'react';
 import {Navbar} from './Navbar';
 import {Board} from './Board';
-import type {Board as BoardType, Cell, Row} from '../types';
-
-const generateEmptyBoard = (rows: number, columns: number) => {
-	const constructedBoard: Row[] = [];
-
-	for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
-		const row: Cell[] = [];
-
-		for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
-			row.push(false);
-		}
-
-		constructedBoard.push(row);
-	}
-
-	return constructedBoard;
-};
+import type {Board as BoardType, Row} from '../types';
+import {generateEmptyBoard, getBoardAfterCellToggle, getBoardAfterGeneration, getNeighbours} from '../utils';
 
 export const App = () => {
 	const [boardXValue, setBoardXValue] = useState('9');
@@ -41,62 +26,13 @@ export const App = () => {
 
 		clearBoard();
 	}, [boardY, boardX]);
-
-	const getNeighbours = (cellIndex: number, rowIndex: number): number => {
-		let neighbours = 0;
-		for (let y = -1; y <= 1; y++) {
-			const newY = rowIndex + y;
-			
-			if (newY < 0 || newY >= boardY) continue;
-			
-			for (let x = -1; x <= 1; x++) {
-				const newX = cellIndex + x;
-				
-				// We're not gonna count a cell as a neighbour of itself...
-				if (newY === 0 && newX === 0) continue;
-				
-				if (newX < 0 || newX >= boardX) continue;
-				
-				if (board[newY][newX]) neighbours++;
-			}
-		}
-		
-		return neighbours;
-	};
 	
 	const handleTick = () => {
-		const newBoard: BoardType = [];
-		
-		board.forEach((row, rowIndex) => {
-			const newRow: Row = [];
-			
-			row.forEach((cell, cellIndex) => {
-				const neighbours = getNeighbours(cellIndex, rowIndex);
-				
-				const isAliveAndNotOverpopulated = cell && (neighbours === 2 || neighbours === 3);
-				const isDeadAndNextToThreeNeighbours = !cell && neighbours === 3; 
-				
-				if (isAliveAndNotOverpopulated || isDeadAndNextToThreeNeighbours) {
-					newRow.push(true);
-				} else {
-					newRow.push(false);
-				}
-			});
-			
-			newBoard.push(newRow);
-		});
-
-		setBoard(newBoard);
+		setBoard(getBoardAfterGeneration);
 	};
 
-	const toggleCell = (row: number, cellIndex: number) => {
-		const boardClone = [...board];
-		const rowClone = [...boardClone[row]];
-
-		rowClone[cellIndex] = !rowClone[cellIndex];
-		boardClone[row] = rowClone;
-
-		setBoard(boardClone);
+	const toggleCell = (rowIndex: number, cellIndex: number) => {
+		setBoard(getBoardAfterCellToggle(board, rowIndex, cellIndex));
 	};
 
 	return <div className="h-full flex flex-col">
---
+++ src/utils.ts
@@ -0,0 +1,77 @@
+import {Board, Board as BoardType, Cell, Row} from './types';
+
+export const generateEmptyBoard = (rows: number, columns: number): Board => {
+	const constructedBoard: Row[] = [];
+
+	for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+		const row: Cell[] = [];
+
+		for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+			row.push(false);
+		}
+
+		constructedBoard.push(row);
+	}
+
+	return constructedBoard;
+};
+
+export const getNeighbours = (board: BoardType, cellIndex: number, rowIndex: number): number => {
+	const maxY = board.length;
+	const maxX = board[0]?.length ?? 0;
+
+	let neighbours = 0;
+	for (let y = -1; y <= 1; y++) {
+		const newY = rowIndex + y;
+
+		if (newY < 0 || newY >= maxY) continue;
+
+		for (let x = -1; x <= 1; x++) {
+			const newX = cellIndex + x;
+
+			// We're not gonna count a cell as a neighbour of itself...
+			if (newY === 0 && newX === 0) continue;
+
+			if (newX < 0 || newX >= maxX) continue;
+
+			if (board[newY][newX]) neighbours++;
+		}
+	}
+
+	return neighbours;
+};
+
+export const getBoardAfterGeneration = (board: Board): Board => {
+	const newBoard: BoardType = [];
+
+	board.forEach((row, rowIndex) => {
+		const newRow: Row = [];
+
+		row.forEach((cell, cellIndex) => {
+			const neighbours = getNeighbours(board, cellIndex, rowIndex);
+
+			const isAliveAndNotOverpopulated = cell && (neighbours === 2 || neighbours === 3);
+			const isDeadAndNextToThreeNeighbours = !cell && neighbours === 3;
+
+			if (isAliveAndNotOverpopulated || isDeadAndNextToThreeNeighbours) {
+				newRow.push(true);
+			} else {
+				newRow.push(false);
+			}
+		});
+
+		newBoard.push(newRow);
+	});
+
+	return newBoard;
+};
+
+export const getBoardAfterCellToggle = (board: Board, cellIndex: number, rowIndex: number): Board => {
+	const boardClone = [...board];
+	const rowClone = [...boardClone[rowIndex]];
+
+	rowClone[cellIndex] = !rowClone[cellIndex];
+	boardClone[rowIndex] = rowClone;
+
+	return boardClone;
+};
