1da1e5c8764d9fdf3b3fa4b01668ecb92ae32598
bigbeno37 <contact@benosullivan.com>
2022-05-06 19:25:27 +1000

    Refactored Navbar to be a fully functional component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,9 @@ import {BoardSizeControls} from './BoardSizeControls';
 import {BoardControls} from './BoardControls';
 import {PlaybackRateControls} from './PlaybackRateControls';
 
-export const Navbar = () => {
-
-	return <nav className="flex flex-row justify-between items-center h-16 bg-gray-400 px-4">
+export const Navbar = () =>
+	<nav className="flex flex-row justify-between items-center h-16 bg-gray-400 px-4">
 		<BoardSizeControls />
 		<BoardControls />
 		<PlaybackRateControls />
 	</nav>;
-};
